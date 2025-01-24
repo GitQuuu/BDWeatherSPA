9fc1943859fb79fed2112576b85f0ae3a2e2c97e
Qu <qu@peer-performance.com>
2025-01-24 22:50:42 +0100

    add the new component to routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import {WeatherDisplayComponent} from './weather-display/weather-display.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'weather', component: WeatherDisplayComponent },
+];
