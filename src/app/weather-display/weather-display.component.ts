2bf4d94f1456ee6a6d9259d17b1b4dd867bb64a9
Qu <qu@peer-performance.com>
2025-01-25 00:25:58 +0100

    prevent memoryleaks

--- src/app/weather-display/weather-display.component.ts
+++ src/app/weather-display/weather-display.component.ts
@@ -1,7 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {WeatherService} from '../services/weather/weather.service';
 import {ApiResponseModel} from '../services/apiResponseModel';
 import {WeatherCurrent} from '../services/weather/weatherCurrentModel';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-weather-display',
@@ -10,16 +11,20 @@ import {WeatherCurrent} from '../services/weather/weatherCurrentModel';
   templateUrl: './weather-display.component.html',
   styleUrl: './weather-display.component.css'
 })
-export class WeatherDisplayComponent implements OnInit {
+export class WeatherDisplayComponent implements OnInit, OnDestroy {
+  private weatherDataSub: Subscription = new Subscription();
     constructor(private weatherService: WeatherService) {
     }
 
+  ngOnDestroy(): void {
+        this.weatherDataSub.unsubscribe();
+    }
+
     ngOnInit(): void {
-        this.weatherService.getWeatherData("Aalborg", 2).subscribe({
+        this.weatherDataSub = this.weatherService.getWeatherData("Aalborg", 2).subscribe({
           next: (data) => {
             let response = data.body as ApiResponseModel;
             this.weatherService.$CurrentDay.set(response.data.current as WeatherCurrent);
-            console.log(this.weatherService.$CurrentDay());
           },
           error: (err) => {
             console.log(err);
