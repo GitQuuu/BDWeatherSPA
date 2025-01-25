0163c47b116dbc360d230bdc3fb066ca0db1c8c6
Qu <qu@peer-performance.com>
2025-01-25 03:01:56 +0100

    Use input to display data dynamically

--- src/app/weather-display/current-day/current-day.component.ts
+++ src/app/weather-display/current-day/current-day.component.ts
@@ -1,16 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {Card} from 'primeng/card';
 import {Button} from 'primeng/button';
+import {CurrentDay, LocationModel} from '../../services/weather/forecastResponseModel';
+import {DatePipe} from '@angular/common';
 
 @Component({
   selector: 'app-current-day',
   imports: [
     Card,
-    Button
+    Button,
+    DatePipe
   ],
   templateUrl: './current-day.component.html',
   styleUrl: './current-day.component.css'
 })
 export class CurrentDayComponent {
-
+  @Input() currentDay!: CurrentDay;
+  @Input() location!: LocationModel;
 }
