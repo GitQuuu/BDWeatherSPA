8f4d1224725e3c1063a3e541f65c2b79639389f6
Qu <qu@peer-performance.com>
2025-01-25 13:32:39 +0100

    add chart.js to app and start with test data

--- src/app/weather-display/current-day/current-day.component.ts
+++ src/app/weather-display/current-day/current-day.component.ts
@@ -1,23 +1,106 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {ChangeDetectorRef, Component, EventEmitter, inject, Input, OnInit, Output, PLATFORM_ID} from '@angular/core';
 import {Card} from 'primeng/card';
 import {Button} from 'primeng/button';
 import {CurrentDay, LocationModel} from '../../services/weather/forecastResponseModel';
-import {DatePipe} from '@angular/common';
+import {DatePipe, isPlatformBrowser} from '@angular/common';
+import {ChartModule, UIChart} from 'primeng/chart';
 
 @Component({
   selector: 'app-current-day',
+  standalone: true,
   imports: [
     Card,
     Button,
-    DatePipe
+    DatePipe,
+    UIChart,
+  ],
+  providers: [
+    ChartModule
   ],
   templateUrl: './current-day.component.html',
   styleUrl: './current-day.component.css'
 })
-export class CurrentDayComponent {
+export class CurrentDayComponent implements OnInit {
+  constructor(private cd: ChangeDetectorRef) {
+  }
+  ngOnInit(): void {
+    this.initChart();
+  }
   @Input() currentDay!: CurrentDay;
   @Input() location!: LocationModel;
   @Output() changeLocation = new EventEmitter();
+  data: any;
+  options: any;
+  platformId = inject(PLATFORM_ID);
+
+  initChart() {
+    if (isPlatformBrowser(this.platformId)) {
+      const documentStyle = getComputedStyle(document.documentElement);
+      const textColor = documentStyle.getPropertyValue('--p-text-color');
+      const textColorSecondary = documentStyle.getPropertyValue('--p-text-muted-color');
+      const surfaceBorder = documentStyle.getPropertyValue('--p-content-border-color');
+
+      this.data = {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [
+          {
+            type: 'line',
+            label: 'Dataset 1',
+            borderColor: documentStyle.getPropertyValue('--p-orange-500'),
+            borderWidth: 2,
+            fill: false,
+            tension: 0.4,
+            data: [50, 25, 12, 48, 56, 76, 42]
+          },
+          {
+            type: 'bar',
+            label: 'Dataset 2',
+            backgroundColor: documentStyle.getPropertyValue('--p-gray-500'),
+            data: [21, 84, 24, 75, 37, 65, 34],
+            borderColor: 'white',
+            borderWidth: 2
+          },
+          {
+            type: 'bar',
+            label: 'Dataset 3',
+            backgroundColor: documentStyle.getPropertyValue('--p-cyan-500'),
+            data: [41, 52, 24, 74, 23, 21, 32]
+          }
+        ]
+      };
+
+      this.options = {
+        maintainAspectRatio: false,
+        aspectRatio: 0.6,
+        plugins: {
+          legend: {
+            labels: {
+              color: textColor
+            }
+          }
+        },
+        scales: {
+          x: {
+            ticks: {
+              color: textColorSecondary
+            },
+            grid: {
+              color: surfaceBorder
+            }
+          },
+          y: {
+            ticks: {
+              color: textColorSecondary
+            },
+            grid: {
+              color: surfaceBorder
+            }
+          }
+        }
+      };
+      this.cd.markForCheck();
+    }
+  }
 
   onChangeLocationClick() {
     console.log(this.currentDay);
