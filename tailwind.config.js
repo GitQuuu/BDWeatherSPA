a728abfad5eae0bbb9535abaa36bd7461699031d
Qu <qu@peer-performance.com>
2025-01-26 01:07:45 +0100

    add richer transtion effect for background colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,9 +10,15 @@ module.exports = {
           '0%': { transform: 'rotateY(0deg)' },
           '100%': { transform: 'rotateY(360deg)' },
         },
+        gradientFade: {
+          '0%': { opacity: 0.6 },
+          '50%': { opacity: 1 },
+          '100%': { opacity: 0.6 },
+        },
       },
       animation: {
         'horizontal-spin': 'horizontalSpin 4s linear infinite',
+        'fade-bg': 'gradientFade 3s ease-in-out infinite',
       },
     },
   },
